35261143db88519d208ea89ded5d100d6dc1b0f1
Admin <Admin@DESKTOP-IJ4V5DK>
2022-09-26 19:24:52 +0530

    First Commit

---
+++ Day-3_DepenedancyInjection/src/main/java/com/cg/diadv/StudentNew.java
@@ -0,0 +1,13 @@
+package com.cg.diadv;
+
+public class StudentNew {
+	MathCheat math;
+	public void cheating() 
+	{
+		math.mathCheat();
+	}
+	
+	public void setMath(MathCheat math) {
+		this.math = math;
+	}
+}
